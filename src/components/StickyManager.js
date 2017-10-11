88b85bc792813e0b76e67b354637df3c470ae358
alymayoung <alymayoung@gmail.com>
2017-10-10 20:59:27 -0700

    Clean up

--- src/components/StickyManager.js
+++ src/components/StickyManager.js
@@ -1,51 +1,32 @@
 import React, {Component} from 'react'
 import ReactDom from 'react-dom'
 import StickyNote from './StickyNote'
-
 class StickyManager extends Component
 {
-   state={
+   state=
+   {
         numberOfStickyNotes : 0
-
    }
-
-
-
     addStickyNote = ()=>{
-
-        console.log("Add sticky");
         this.setState(
-           { numberOfStickyNotes : (this.state.numberOfStickyNotes + 1) }
-
-
+            { numberOfStickyNotes : (this.state.numberOfStickyNotes + 1) }
         );
-
     }
-
     render()
     {
-        const stickyNotes = [];
-        
+        const stickyNotes = [];   
         for (var i = 0; i < this.state.numberOfStickyNotes; i += 1) 
         {
             stickyNotes.push(<StickyNote key={i} number={i} />);
         };
-
-
         return(
-            <div>
-                
-                <button onClick={this.addStickyNote}>Add</button> | Hide/Show
+            <div>               
+               <button onClick={this.addStickyNote}>Add</button> | Hide/Show
                 <div>
                     {stickyNotes}
                 </div>
             </div>
-
         );
-
     }
-
-
 }
-
 export default StickyManager;
--- src/components/StickyNote.js
+++ src/components/StickyNote.js
@@ -1,37 +1,26 @@
 import React, {Component} from 'react'
 import ReactDom from 'react-dom'
 import Draggable from 'react-draggable';
-
 require('./StickyNote.css');
-
-
 class StickyNote extends Component
 {
     render()
     {
        return(
-
         <Draggable
-       
         handle=".handle"
         defaultPosition={{x: 0, y: 0}}
         position={null}
-        
         onStart={this.handleStart}
         onDrag={this.handleDrag}
         onStop={this.handleStop}
-        
-        >
+         >
         <div>
           <div className="handle stickyNote">Drag from here</div>
           <div>This readme is really dragging on...</div>
         </div>
       </Draggable>
        ); 
-
     }
-
-
 }
-
 export default StickyNote;
